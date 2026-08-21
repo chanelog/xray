242ea6fe28877666cc8527680ca6f7a7e4994632
chanelog <chanelog93@gmail.com>
2026-08-21 16:16:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -922,9 +922,11 @@ install_script_files() {
   chmod +x $BIN_DIR/vpn
 
   cat > $SCRIPT_DIR/banner.txt <<'EOF2'
-  ╔══════════════════════════════════════╗
-  ║         CHANELOG VPN TUNNEL SERVER          ║
-  ╚══════════════════════════════════════╝
+  CHANELOG VPN TUNNEL SERVER
+        NO HACKING
+        NO DDOS
+        NO VOKEP
+  GUNAKAN SEBIJAK MUNGKIN
 EOF2
 
   echo -e "${GREEN}[OK]${NC} Script files terinstall"
